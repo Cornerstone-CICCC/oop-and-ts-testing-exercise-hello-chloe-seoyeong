d7fb603467d66aa86fa7ca49d3720790a6c0bd25
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-01-23 12:21:15 -0800

    Completed exercise testing

--- tests/counter.test.js
+++ tests/counter.test.js
@@ -1,7 +1,24 @@
 // Create two test cases for incrementing and decrementing a number
-// Test case 1: counter(10, 'increment') will eventualy return 10
+// Test case 1: counter(1, 'increment') will eventualy return 10
 // Test case 2: counter(10, 'decrement') will eventually return 0
 // Both test cases need to pass
 // HINT: Use the jest `resolves` matcher so that it is easier to work with async functions
 
 const counter = require('../src/counter')
+
+// test('10, increment should be return 10', () => {
+//   return expect(counter(10, "increment")).resolves.toBe(10);
+// }, 11000)
+
+// test('10, decrement should be return 0', () => {
+//   return expect(counter(10, "decrement")).resolves.toBe(0);
+// }, 11000)
+
+test('5, increment should be return 5', () => {
+  return expect(counter(3, "increment")).resolves.toBe(3);
+})
+
+test('3, decrement should be return 0', () => {
+  return expect(counter(3, "decrement")).resolves.toBe(0);
+})
+
